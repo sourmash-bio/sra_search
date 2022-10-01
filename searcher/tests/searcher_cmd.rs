ee7d6128a3a1019aaf6c372dd6cda1e0c542196e
Luiz Irber <contact+github@luizirber.org>
2022-10-01 11:41:06 -0700

    add basic tests

---
+++ searcher/tests/searcher_cmd.rs
@@ -0,0 +1,59 @@
+use std::fs;
+use std::io::{Read, Write};
+use std::process::Command;
+
+use assert_cmd::prelude::*;
+use predicates::prelude::PredicateBooleanExt;
+use predicates::str::contains;
+use tempfile::NamedTempFile;
+
+#[test]
+fn search() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("searcher")?;
+
+    let mut queries = NamedTempFile::new()?;
+    writeln!(queries, "tests/data/genome-s10.fa.gz.sig")?;
+
+    let mut catalog = NamedTempFile::new()?;
+    writeln!(catalog, "tests/data/genome-s10.fa.gz.sig")?;
+    writeln!(catalog, "tests/data/genome-s11.fa.gz.sig")?;
+    writeln!(catalog, "tests/data/genome-s12.fa.gz.sig")?;
+
+    cmd.args(&["--threshold", "0"])
+        .args(&["-k", "31"])
+        .args(&["--scaled", "10000"])
+        .arg(queries.path())
+        .arg(catalog.path())
+        .assert()
+        .success()
+        .stdout(contains("query,Run,containment"))
+        .stdout(contains(
+            "../genome-s10.fa.gz','tests/data/genome-s10.fa.gz.sig',1",
+        ));
+
+    Ok(())
+}
+
+#[test]
+fn search_empty_query() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("searcher")?;
+
+    let queries = NamedTempFile::new()?;
+
+    let mut catalog = NamedTempFile::new()?;
+    writeln!(catalog, "tests/data/genome-s10.fa.gz.sig")?;
+    writeln!(catalog, "tests/data/genome-s11.fa.gz.sig")?;
+    writeln!(catalog, "tests/data/genome-s12.fa.gz.sig")?;
+
+    cmd.args(&["--threshold", "0"])
+        .args(&["-k", "31"])
+        .args(&["--scaled", "10000"])
+        .arg(queries.path())
+        .arg(catalog.path())
+        .assert()
+        .success()
+        .stderr(contains("No query signatures loaded, exiting."))
+        .stdout(contains("query,Run,containment").not())
+
+    Ok(())
+}
