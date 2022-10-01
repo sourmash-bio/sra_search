7e7f493f2930a5de8ea18561acfc8a38965381e0
Luiz Irber <contact+github@luizirber.org>
2022-10-01 16:19:27 -0700

    update nix

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "utils";
+      };
+    };
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        rustVersion = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = rustVersion;
+          rustc = rustVersion;
+        };
+      in
+
+      with pkgs;
+      {
+        devShell = mkShell {
+          nativeBuildInputs = [
+            clang_13
+          ];
+
+          buildInputs = [
+            rustPlatform.rust.cargo
+            openssl
+            pkg-config
+
+            git
+            stdenv.cc.cc.lib
+
+            snakemake
+          ];
+
+          LD_LIBRARY_PATH = "${stdenv.cc.cc.lib}/lib64:$LD_LIBRARY_PATH";
+
+          # workaround for https://github.com/NixOS/nixpkgs/blob/48dfc9fa97d762bce28cc8372a2dd3805d14c633/doc/languages-frameworks/python.section.md#python-setuppy-bdist_wheel-cannot-create-whl
+          SOURCE_DATE_EPOCH = 315532800; # 1980
+        };
+      });
+}
--- shell.nix
+++ shell.nix
@@ -1,27 +1,10 @@
-let
-  sources = import ./nix/sources.nix;
-  rustPlatform = import ./nix/rust.nix { inherit sources; };
-  pkgs = import sources.nixpkgs { overlays = [ (import sources.rust-overlay) ]; };
-in
-  with pkgs;
-
-  pkgs.mkShell {
-    nativeBuildInputs = [
-      clang_13
-    ];
-
-    buildInputs = [
-      rustPlatform.rust.cargo
-      openssl
-      pkg-config
-
-      git
-      stdenv.cc.cc.lib
-      (python310.withPackages(ps: with ps; [ virtualenv tox setuptools ]))
-
-      cargo-outdated
-
-      llvmPackages_13.libclang
-      llvmPackages_13.libcxxClang
-    ];
-  }
+(import
+  (
+    fetchTarball {
+      url = "https://github.com/edolstra/flake-compat/archive/99f1c2157fba4bfe6211a321fd0ee43199025dbf.tar.gz";
+      sha256 = "0x2jn3vrawwv9xp15674wjz9pixwjyj3j771izayl962zziivbx2";
+    }
+  )
+  {
+    src = ./.;
+  }).shellNix
